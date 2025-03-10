f40a92938a7b1071caffcf7813407e15f20aa9d7
Dmitry <Ardenna110@yandex.ru>
2025-03-10 05:03:28 +0300

    Update FilmStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,11 +6,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
-	List<Film> getFilms();
+List<Film> getFilms();
 
-    Film createFilm(Film film);
+Film createFilm(Film film);
+
+Film updateFilm(Film film);
 	
-	Film updateFilm(Film film);
-	
-	Optional<Film> getFilmById(Long userId);
+Optional<Film> getFilmById(Long userId);
 }
