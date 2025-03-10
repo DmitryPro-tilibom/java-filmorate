6b8b24cc1ef47a1589efbbd6f241c22f4b85c26d
Dmitry <Ardenna110@yandex.ru>
2025-03-10 05:46:08 +0300

    Update UserStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,12 +6,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
-    
     List<User> getUsers();
-    
+
     User createUser(User user);
-    
+
     User updateUser(User user);
-    
+
     Optional<User> getUserById(Long userId);
 }
